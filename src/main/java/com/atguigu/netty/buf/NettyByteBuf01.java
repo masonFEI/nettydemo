db6c2d20a79cc9bbaa78c73352309bbfb99e743b
masonFei <994085848@qq.com>
2023-12-07 23:38:25 +0800

    nettyByteBuf01

---
+++ src/main/java/com/atguigu/netty/buf/NettyByteBuf01.java
@@ -0,0 +1,31 @@
+package com.atguigu.netty.buf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class NettyByteBuf01 {
+
+
+    public static void main(String[] args) {
+
+
+        ByteBuf buffer = Unpooled.buffer(10);
+
+        for (int i = 0; i < 10; i++) {
+            buffer.writeByte(i);
+        }
+
+        System.out.println("capacity=" + buffer.capacity());
+
+
+        for (int i = 0; i < buffer.capacity(); i++) {
+            System.out.println(buffer.readByte());
+        }
+
+        System.out.println("执行完毕");
+
+
+    }
+
+
+}
