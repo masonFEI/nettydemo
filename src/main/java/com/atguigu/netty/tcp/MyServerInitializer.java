9fbbc7762036cd1d32026794675e7ee4a4ae11e8
masonFei <994085848@qq.com>
2024-01-01 22:06:04 +0800

    tcp粘包拆包demo

---
+++ src/main/java/com/atguigu/netty/tcp/MyServerInitializer.java
@@ -0,0 +1,15 @@
+package com.atguigu.netty.tcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        // 自定义处理方法
+        pipeline.addLast(new MyServerHandler());
+        System.out.println("xx");
+    }
+}
