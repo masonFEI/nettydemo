41191c96f3f8836215f4767234298f3b96e29047
masonFei <994085848@qq.com>
2024-01-04 22:43:56 +0800

    Dubbo RPC

---
+++ src/main/java/com/atguigu/netty/dubborpc/netty/MyServerHandler.java
@@ -0,0 +1,45 @@
+package com.atguigu.netty.dubborpc.netty;
+
+import com.atguigu.netty.protocoltcp.MessageProtocol;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class MyServerHandler extends SimpleChannelInboundHandler<MessageProtocol> {
+
+    private int count;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MessageProtocol msg) throws Exception {
+
+        // 接收到数据并处理
+        int len = msg.getLen();
+        byte[] content = msg.getContent();
+
+        System.out.println("服务器接收到消息如下");
+        System.out.println("长度=" + len);
+        System.out.println("内容=" + new String(content, StandardCharsets.UTF_8));
+
+        System.out.println("服务器接收到消息包数量=" + (++this.count));
+
+        System.out.println("服务端开始回复消息------");
+        String responseContent = UUID.randomUUID().toString();
+        int responseLen = responseContent.getBytes(StandardCharsets.UTF_8).length;
+        byte[] responseContent2 = responseContent.getBytes(StandardCharsets.UTF_8);
+        // 构建一个协议包
+        MessageProtocol messageProtocol = new MessageProtocol();
+        messageProtocol.setLen(responseLen);
+        messageProtocol.setContent(responseContent2);
+
+        ctx.writeAndFlush(messageProtocol);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ src/main/java/com/atguigu/netty/dubborpc/netty/NettyServer.java
@@ -0,0 +1,43 @@
+package com.atguigu.netty.dubborpc.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+
+    public static void startServer(String hostName,int port) {
+
+    }
+
+
+    public static void startServer0(String hostName,int port) throws Exception {
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer());
+
+            System.out.println("...服务器 is ready...");
+            // 绑定一个端口并且同步
+            // 启动服务器并绑定端口
+            ChannelFuture cf = bootstrap.bind(7000).sync();
+            // 对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+
+
+}
